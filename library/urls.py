abd07bd8a74514d7b5829190f013a614314b0bbd
Evgeniy Gaydarenko <62751528+Gaydar-Kanub@users.noreply.github.com>
2020-11-07 10:42:57 +0300

    release urls

--- library/urls.py
+++ library/urls.py
@@ -1,6 +1,7 @@
-# from django.urls import path
-# from .views import
-#
-# urlpatterns = [
-#     path('', )
-# ]
+from django.urls import path
+from .views import BookListView, BookView
+
+urlpatterns = [
+    path('books/', BookListView.as_view()),
+    path('books/<int:book_id>', BookView.as_view()),
+]
