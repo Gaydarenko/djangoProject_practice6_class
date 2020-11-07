5681c15f552a485c12c615a2c82c78c16f6ca489
Evgeniy Gaydarenko <62751528+Gaydar-Kanub@users.noreply.github.com>
2020-11-07 10:36:42 +0300

    release def get in BooksListView

--- library/views.py
+++ library/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.views import View
+from django.forms import model_to_dict
+
+from.models import Book
 
 
 # Create your views here.
 class BookListView(View):
     def get(self, request):
-        ...
+        books = Book.objects.all()
+        return JsonResponse([model_to_dict(book) for book in books], safe=False)
 
     def post(self, request):
         ...
